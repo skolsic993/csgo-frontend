33244f4eb42081030791d5fffe4cd66b8533b6fb
Miloš Stojsavljević <skolsic993@gmail.com>
2023-03-20 19:24:25 +0100

    Add sakai light theme to the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
-import { AuthGuard } from './auth/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { HomeComponent } from './home/main/home.component';
+import { AppLayoutComponent } from './layout/app.layout.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children: [{ path: '', component: HomeComponent }],
+  },
   {
     path: 'matches',
     canLoad: [AuthGuard],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { HomeModule } from './home/home.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeModule } from './home/home.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AppLayoutModule } from './layout/app.layout.module';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, HomeModule, AppLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
